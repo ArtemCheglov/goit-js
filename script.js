1cff4a65c0e4e2d70a462b1294a520ec8e40e5de
ArtemCheglov <3366artem@gmail.com>
2025-04-23 18:35:08 +0200

    d

--- script.js
+++ script.js
@@ -822,3 +822,38 @@
 //     email: 'jonh@example.com'
 // };
 // const { name, age, email } = user;
+
+// const players = [
+//     { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//     { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//     { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//     { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
+
+// // Отримати масив імен всіх гравців.
+// const playersNames = players.map((player) => player.name);
+// console.log(playersNames);
+
+// // Отримати масив всіх гравців онлайн.
+// const playersOnline = players.filter((player) => player.online);
+// console.log(playersOnline);
+
+// // Збільшити кількість поінтів кожного гравця на 10% 
+// // (розпорошуємо старий об 'єкт).
+// const pointsUpdate = players.map((player) => {
+//     return {
+//         ...player,
+//         points: Math.round(player.points * 1.1)
+//     }
+// })
+// console.log(pointsUpdate);
+
+// // Знайти гравця по імені.
+// const findPlayer = players.find((player) => player.name === "Kiwi");
+// console.log(findPlayer);
+
+// function findPlayerByName(arr, name) {
+//     return arr.find(player => player.name === name)
+// }
+// console.log(findPlayerByName(players, "Ajax"));
