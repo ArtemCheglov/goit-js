52b2189dd930f97ac16d6a32bdb127aa627d8b51
ArtemCheglov <3366artem@gmail.com>
2025-05-05 17:09:18 +0200

    comit

---
+++ script15.js
@@ -0,0 +1,28 @@
+//1
+const calculateTotalBalance = users => {
+
+};
+
+console.log(calculateTotalBalance(users));
+
+//2
+const getUsersWithFriend = (users, friendName) => {
+
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker'));
+console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+
+//3
+const getNamesSortedByFriendsCount = users => {
+
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+
+//4
+const getSortedUniqueSkills = users => {
+
+};
+
+console.log(getSortedUniqueSkills(users));
