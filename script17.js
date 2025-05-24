95073eb13a490e3c9647d756cff29aae663ac426
ArtemCheglov <3366artem@gmail.com>
2025-05-25 00:55:13 +0200

    comit

---
+++ script17.js
@@ -0,0 +1,27 @@
+//1
+const batton = document.querySelector("button")
+const input = document.querySelector("input")
+
+function replace(input, button) {
+    button.textContent = input.value;
+}
+
+replace()
+
+//2
+const img = document.querySelector("img")
+console.log(img);
+img.src = "https://www.bing.com/images/search?view=detailV2&ccid=oR9uodk%2f&id=A4BCE5F15D99E43775994AD3F819B87399ABA010&thid=OIP.oR9uodk_3J-vacWsJpK7AgHaHa&mediaurl=https%3a%2f%2fi.etsystatic.com%2f8903875%2fr%2fil%2f150a9e%2f5143350608%2fil_fullxfull.5143350608_f0ww.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.a11f6ea1d93fdc9faf69c5ac2692bb02%3frik%3dEKCrmXO4GfjTSg%26pid%3dImgRaw%26r%3d0&exph=2000&expw=2000&q=Happy+Cat+Banana&simid=608011063487257285&FORM=IRPRST&ck=786D998A41C7238209FEE55120607EC0&selectedIndex=1&itb=0&ajaxhist=0&ajaxserp=0"
+
+//3
+const links = document.querySelector("a[href]")
+console.log(links);
+link.href = "https://www.bing.com/images/search?view=detailV2&ccid=oR9uodk%2f&id=A4BCE5F15D99E43775994AD3F819B87399ABA010&thid=OIP.oR9uodk_3J-vacWsJpK7AgHaHa&mediaurl=https%3a%2f%2fi.etsystatic.com%2f8903875%2fr%2fil%2f150a9e%2f5143350608%2fil_fullxfull.5143350608_f0ww.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.a11f6ea1d93fdc9faf69c5ac2692bb02%3frik%3dEKCrmXO4GfjTSg%26pid%3dImgRaw%26r%3d0&exph=2000&expw=2000&q=Happy+Cat+Banana&simid=608011063487257285&FORM=IRPRST&ck=786D998A41C7238209FEE55120607EC0&selectedIndex=1&itb=0&ajaxhist=0&ajaxserp=0"
+
+const img1 = document.querySelector("img1")
+console.log(img1);
+img1.alt = "new"
+
+//4
+const firstEl = document.querySelector("list")
+firstEl.textContent = "4"
