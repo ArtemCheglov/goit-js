953b47cc3e10809ae2ccc2d5fd74eb5f1d248d8c
ArtemCheglov <3366artem@gmail.com>
2025-05-26 16:59:38 +0200

    comit

--- script17.js
+++ script17.js
@@ -1,9 +1,12 @@
 //1
+batton.classList.add("batton")
 const batton = document.querySelector("button")
+
+input.classList.add("input")
 const input = document.querySelector("input")
 
-function replace(input, button) {
-    button.textContent = input.value;
+function replace() {
+    batton.textContent = input.value;
 }
 
 replace()
@@ -14,15 +17,15 @@ img.src = "https://www.bing.com/images/search?view=detailV2&ccid=oR9uodk%2f&id=A
 console.log(img);
 
 //3
-const links = document.querySelector("a[href]")
+const link = document.querySelector("a[href]")
 link.href = "https://www.bing.com/images/search?view=detailV2&ccid=oR9uodk%2f&id=A4BCE5F15D99E43775994AD3F819B87399ABA010&thid=OIP.oR9uodk_3J-vacWsJpK7AgHaHa&mediaurl=https%3a%2f%2fi.etsystatic.com%2f8903875%2fr%2fil%2f150a9e%2f5143350608%2fil_fullxfull.5143350608_f0ww.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.a11f6ea1d93fdc9faf69c5ac2692bb02%3frik%3dEKCrmXO4GfjTSg%26pid%3dImgRaw%26r%3d0&exph=2000&expw=2000&q=Happy+Cat+Banana&simid=608011063487257285&FORM=IRPRST&ck=786D998A41C7238209FEE55120607EC0&selectedIndex=1&itb=0&ajaxhist=0&ajaxserp=0"
-console.log(links);
+console.log(link);
 
-const img1 = document.querySelector("img1")
-img1.alt.add = "new"
-console.log(img1);
+const imgEl = document.querySelector("img1")
+imgEl.alt = "new"
+console.log(imgEl);
 
 //4
 const firstEl = document.querySelector("list")
-firstEl.textContent = "4"
+firstEl.firstChild.textContent = "4";
 console.log(firstEl);
